5440d3adfafd1957ed893a6f02b0427f54fa7f23
Kristin Caron <krissycaron@gmail.com>
2017-08-03 10:38:01 -0500

    got through number 19

--- top_2009_agent.sql
+++ top_2009_agent.sql
@@ -5,8 +5,16 @@
 select
 	max(Sales.TotalSales) as TopSales,
 	Sales.EmployeeName
-	from
-	(select
-		sum(i.Total) TOtalSales,
-		
+from 
+	(select 
+	sum(i.total) as "TotalSales",
+	e.FirstName || " " || e.LastName as "EmployeeName"
+	from Invoice i, Customer c, Employee e
+	where i.customerid = c.customerid
+	and c.supportrepId = e.employeeid
+	and e.title = "Sales Support Agent"
+	and i.InvoiceDate like '%2009%'
+	group by "Sales Representative"
 	)
+	as Sales;
+
